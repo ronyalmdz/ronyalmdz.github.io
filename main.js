9e1bfac7146106378eb0a2ce21a1dfa68bbbec27
Rony <almendarez93@gmail.com>
2017-04-16 23:34:29 -0600

    cambios en js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 
-$(document).ready(function () {	
+jQuery(document).ready(function () {	
 
     // permitir el evento de scroll sobre el mapa al hacer clic;
 
-    $('#map_canvas1').addClass('scrolloff'); // apagar el evento de scroll sobre el mapa
-    $('#canvas1').on('click', function () {
-        $('#map_canvas1').removeClass('scrolloff'); // encender el evento de scroll al hacer clic
+    jQuery('#map_canvas1').addClass('scrolloff'); // apagar el evento de scroll sobre el mapa
+    jQuery('#canvas1').on('click', function () {
+        jQuery('#map_canvas1').removeClass('scrolloff'); // encender el evento de scroll al hacer clic
     });
 
     // deshabilitar el evento cuando haces clic
 
-    $("#map_canvas1").mouseleave(function () {
-        $('#map_canvas1').addClass('scrolloff'); // quitar el cursor pointer cuando se mueve el mouse 
+    jQuery("#map_canvas1").mouseleave(function () {
+        jQuery('#map_canvas1').addClass('scrolloff'); // quitar el cursor pointer cuando se mueve el mouse 
     });
 });
 

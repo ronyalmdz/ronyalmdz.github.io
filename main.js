25060c5765b0194be99e24c5074d771737b83956
Rony <almendarez93@gmail.com>
2017-04-16 23:40:51 -0600

    prueba en index y js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 
-jQuery(document).ready(function () {	
+$(document).ready(function () {	
 
     // permitir el evento de scroll sobre el mapa al hacer clic;
 
-    jQuery('#map_canvas1').addClass('scrolloff'); // apagar el evento de scroll sobre el mapa
-    jQuery('#canvas1').on('click', function () {
-        jQuery('#map_canvas1').removeClass('scrolloff'); // encender el evento de scroll al hacer clic
+    $('#map_canvas1').addClass('scrolloff'); // apagar el evento de scroll sobre el mapa
+    $('#canvas1').on('click', function () {
+        $('#map_canvas1').removeClass('scrolloff'); // encender el evento de scroll al hacer clic
     });
 
     // deshabilitar el evento cuando haces clic
 
-    jQuery("#map_canvas1").mouseleave(function () {
-        jQuery('#map_canvas1').addClass('scrolloff'); // quitar el cursor pointer cuando se mueve el mouse 
+    $("#map_canvas1").mouseleave(function () {
+        $('#map_canvas1').addClass('scrolloff'); // quitar el cursor pointer cuando se mueve el mouse 
     });
 });
 
